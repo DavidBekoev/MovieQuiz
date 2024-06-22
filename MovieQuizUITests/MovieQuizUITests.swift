d8bad51daed7de19ba210373b886c717da7e2275
David <davidbekoev555@gmal.com>
2024-06-23 00:47:37 +0300

    Начало выноса бызнес логики

--- MovieQuizUITests/MovieQuizUITests.swift
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -60,6 +60,39 @@ import XCTest
             XCTAssertNotEqual(firstPosterData, secondPosterData)
             XCTAssertEqual(indexLabel.label, "2/10")
      }
+     
+     
+     func testGameFinish() {
+         sleep(2)
+         for _ in 1...10 {
+                app.buttons["No"].tap()
+                sleep(2)
+            }
+
+            let alert = app.alerts["Game results"]
+            
+            XCTAssertTrue(alert.exists)
+            XCTAssertTrue(alert.label == "Этот раунд окончен!")
+            XCTAssertTrue(alert.buttons.firstMatch.label == "Сыграть ещё раз")
+     }
+     
+     func testAlertDismiss() {
+         sleep(2)
+         for _ in 1...10 {
+             app.buttons["No"].tap()
+             sleep(2)
+         }
+         
+         let alert = app.alerts["Game results"]
+         alert.buttons.firstMatch.tap()
+         
+         sleep(2)
+         
+         let indexLabel = app.staticTexts["Index"]
+         
+         XCTAssertFalse(alert.exists)
+         XCTAssertTrue(indexLabel.label == "1/10")
+     }
     
     func testExample() throws {
         // UI tests must launch the application that they test.
