e55b7bf53dbbd0c3177387577bb11c2597fb3057
David <davidbekoev555@gmal.com>
2024-06-27 20:59:16 +0300

    Ошибки Исправлены

--- MovieQuizUITests/MovieQuizUITests.swift
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -46,13 +46,13 @@ final class MovieQuizUITests: XCTestCase {
        }
 
        func testNoButton() {
-           sleep(4)
+           sleep(5)
 
            let firstPoster = app.images["Poster"]
            let firstPosterData = firstPoster.screenshot().pngRepresentation
 
            app.buttons["No"].tap()
-           sleep(3)
+           sleep(5)
 
            let secondPoster = app.images["Poster"]
            let secondPosterData = secondPoster.screenshot().pngRepresentation
@@ -74,13 +74,13 @@ final class MovieQuizUITests: XCTestCase {
            let alert = app.alerts["Game Result"]
            XCTAssertTrue(alert.exists)
            XCTAssertTrue(alert.label == "Этот раунд окончен!")
-           XCTAssertEqual(alert.buttons.firstMatch.label, "Сыграть еще раз")
+           XCTAssertEqual(alert.buttons.firstMatch.label, "Сыграть ещё раз")
        }
 
     
   
            func testAlertDismiss() {
-           sleep(2)
+           sleep(5)
 
            for _ in 1...10 {
                app.buttons["Yes"].tap()
@@ -90,7 +90,7 @@ final class MovieQuizUITests: XCTestCase {
            let alert = app.alerts["Game Result"]
            alert.buttons.firstMatch.tap()
 
-           sleep(2)
+           sleep(5)
 
            let indexLabel = app.staticTexts["Index"]
 
